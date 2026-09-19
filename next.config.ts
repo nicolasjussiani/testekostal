d6f23b1cf2c66ddfc9394669e143c3f47bd797e6
nicolasjussiani <nicolas.jussiani@hotmail.com>
2026-09-19 20:02:35 -0300

    fix: remove invalid eslint config from next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,8 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   images: {
-    formats: ["image/avif", "image/webp"],
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
+    formats: ['image/avif', 'image/webp'],
   },
 };
 
